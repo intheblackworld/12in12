c173566a1a7195e207fd35d045fd2a5f7c54b3fb
intheblackworld <intheblackworld@gmail.com>
2015-02-20 18:32:38 +0800

    test

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -70,6 +70,21 @@ end
     end
   end
 
+  def upvote
+    @link = Link.find(params[:id])
+    @user == current_user
+    @post.upvote_by @user
+    redirect_to :back
+  end
+
+
+  def downvote
+    @link = Link.find(params[:id])
+    @user == current_user
+    @post.downvote_by @user
+    redirect_to :back
+  end
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_link
